18f30e745e6239b3eb217aa06a23d01e3733ed2d
Praktikan <Praktikan@Praktikan-PC>
2018-03-27 10:28:54 +0700

    New code

--- src/fibonacci/Fibonacci.java
+++ src/fibonacci/Fibonacci.java
@@ -21,18 +21,21 @@ public class Fibonacci {
         System.out.print("Masukan Jumlah Deret Fibonacci : ");
         int n = show.nextInt();
         long fib[] = new long[n];
+        long x = 0;
          
-        fib[0] = 0;
+        fib[0] = 1;
         fib[1] = 1;
-         
+        
         for(int i = 2; i < n; i++) {
             fib[i] = fib[i-1] + fib[i-2];
-        }
-         
+        }   
+        
         for (int i = 0; i < n; i++) {
-            System.out.print(fib[i] +  " ");
+            x = x + fib[i];
         }
-    }
-}
-    
+            System.out.print(x +  " ");
 
+ 
+    }
+
+}
